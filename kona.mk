55862e4250a1862109a0770d4193bd2168b2c5f4
Eamo5 <Eamo5>
2022-03-05 08:27:52 +0545

    sm8250-common:Add task profiles for API31

--- kona.mk
+++ kona.mk
@@ -102,6 +102,11 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.usbv2.default
 
+# Task profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/task_profiles/cgroups_31.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/cgroups_31.json \
+    $(LOCAL_PATH)/task_profiles/task_profiles_31.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/task_profiles_31.json
+
 PRODUCT_PACKAGES += \
     BluetoothQti \
     liba2dpoffload \
